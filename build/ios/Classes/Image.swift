559d56e6fb71844991f0c7a70af30aff11940187
Carlos Castro <cdcastrofelix@gmail.com>
2019-07-31 20:38:28 -0700

    chore: upgrade dependencies to prevent vulnerabilities

--- build/ios/Classes/Image.swift
+++ build/ios/Classes/Image.swift
@@ -7,10 +7,8 @@ import UIKit
 public enum IconImage: String, CaseIterable
 {
     case delete
-    case edit
     case restart
     case search
-    case settings
     case trash
 
   public var image: UIImage
@@ -19,14 +17,10 @@ public enum IconImage: String, CaseIterable
       {
            case .delete:
               return Image.delete
-           case .edit:
-              return Image.edit
            case .restart:
               return Image.restart
            case .search:
               return Image.search
-           case .settings:
-              return Image.settings
            case .trash:
               return Image.trash
       }
@@ -36,9 +30,7 @@ public enum IconImage: String, CaseIterable
 public class Image
 {
   public static let delete:UIImage = UIImage.image(for: "delete")
-  public static let edit:UIImage = UIImage.image(for: "edit")
   public static let restart:UIImage = UIImage.image(for: "restart")
   public static let search:UIImage = UIImage.image(for: "search")
-  public static let settings:UIImage = UIImage.image(for: "settings")
   public static let trash:UIImage = UIImage.image(for: "trash")
 }
